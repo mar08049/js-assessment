cf11cf9689580d84d0a51d1f4d8d9bf45c59c717
github username <github email address>
2018-04-02 11:57:53 -0700

    updated functions

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -6,19 +6,32 @@ class CommentsController {
   init() {
   }
 
-  CommentsController.prototype.render(commentObject)
+  CommentsController.prototype.render = $(function(){
+    $.getJSON(this.href).success(function(json){ //on success, append comment
+       var $ul = $("div.comments ul"); //set ul var to the comments ul div
+       $ul.html(""); //get the html contents of the variable
+       json.forEach(function(comment){ //for each json, perform function
+         $ul.append("<li>" + comment.description + "</li>"); // appends comment desc in li tags
+  };
 
   // selects the appropriate `ul` for this comment to be added to
   // appends the new comment element to this `ul`
-  // Don't try to copy the `ImagesController.render` function because that is implemented differently
 
-  CommentController.prototype.addCommentFormListener = function(data){
-    $.ajax({
-      type: 'POST',
-      url: '',
-      data: data,
-      headers: {}
-    })
+  CommentController.prototype.addCommentFormListener = function(){
+    let elements = document.getElementsByTagName(':submit')
+    for (let i = 0 < elements.length; i++) {
+      $(':submit').on('click', function(e){
+        e.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: e.target.form.action,
+          data: $(e.target.form).serialize(),
+          dataType: "JSON"
+        }).success(function(response){
+          console.log(response)
+        })
+      })
+    }
   }
   // iterates through each comment form and adds an eventlistener to trigger a function on form submit
   // function should grab the imageId + comment and create a new Comment with those arguments
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,26 +1,26 @@
 // create Comment class here
-class Comment {
-  constructor(comment, imageId) {
-  this.comment = comment;
-  this.imageId = imageId;
+class Comment extends Image {
+  constructor(id, comment, imageId) {
+      super(comments);
+
+      this.comment = comment;
+      this.imageId = imageId;
+    }
   }
 
   allComments() {
-    return Comment.all//get all the comments
+    return this.comment.all//get all the comments
   }
 
   Comment.prototype.commentEl = function() {
     let html = ''
     html += `<li id="comment_el">${this.comment}</li>`
-    return html
-    //returns comment html
-    // returns a string of html
-    // html has an `li` tag with an `id` field and shows the comment
-
+    return `<ul id=${this.imageId}> ` + html + '</ul>'
   }
 
-  Comment.prototype.findImage = function(imageId){
-    // given an `int` for an image id, returns the image object with that id
-    // before return - adds current comment to image's comments property
+  Comment.prototype.findImage = function(){
+    let currentContent = Comment.find(image => image.id === this.imageId)
+    this.comments.push(this.comment)
+    return currentContent
   }
 }
